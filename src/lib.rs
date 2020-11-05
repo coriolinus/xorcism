f0fa427664f0c07e9f7b74f3f8c3c9c49e080ccf
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2020-11-05 10:35:22 +0100

    make writer work
    It's unfortunate that we had to resort to reimplementation; it feels
    like a very strong violation of DRY. We can figure that out later, though.

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,37 @@
 use std::borrow::Borrow;
 use std::io::{self, Write};
-use std::iter::Cycle;
-use std::slice::Iter;
 
 /// A munger which XORs a key with some data
 ///
 /// This is a low-level structure; more often, you'll want to use [`Writer`], [`Reader`], or [`munge`].
 #[derive(Clone)]
 pub struct Xorcism<'a> {
-    key: Cycle<Iter<'a, u8>>,
+    key: &'a [u8],
+    pos: usize,
 }
 
 impl<'a> Xorcism<'a> {
     /// Create a new Xorcism munger from a key
     pub fn new(key: &'a [u8]) -> Xorcism<'a> {
-        Xorcism {
-            key: key.into_iter().cycle(),
-        }
+        Xorcism { key, pos: 0 }
+    }
+
+    fn incr_pos(&mut self, by: usize) -> usize {
+        let old_pos = self.pos;
+        self.pos += by;
+        old_pos
     }
 
     /// XOR each byte of the data with a byte from the key
     pub fn munge<Data, B>(&'a mut self, data: Data) -> impl 'a + Iterator<Item = u8>
     where
         Data: 'a + IntoIterator<Item = B>,
+        <Data as IntoIterator>::IntoIter: ExactSizeIterator,
         B: Borrow<u8>,
     {
-        data.into_iter()
-            .zip(self.key.by_ref())
+        let data = data.into_iter();
+        let pos = self.incr_pos(data.len());
+        data.zip(self.key.iter().cycle().skip(pos))
             .map(|(d, k)| d.borrow() ^ k)
     }
 
@@ -60,6 +65,7 @@ where
 /// It is constructed with [`Xorcism::writer`].
 ///
 /// It does not perform any internal buffering.
+#[derive(Clone)]
 pub struct Writer<'a, W> {
     xorcism: Xorcism<'a>,
     writer: W,
@@ -72,8 +78,23 @@ where
     /// This implementation will block until the underlying writer
     /// has written the entire input buffer.
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        let mut munged = Vec::with_capacity(buf.len());
-        munged.extend(self.xorcism.munge(buf));
+        let munged = {
+            let mut munged = Vec::with_capacity(buf.len());
+            // rustc can't figure out an appropriate lifetime if we use self.xorcism.munge,
+            // so let's work around the issue by reimplementing it. Luckily, it's fairly straightforward.
+            let pos = self.xorcism.incr_pos(buf.len());
+
+            munged.extend(
+                self.xorcism
+                    .key
+                    .iter()
+                    .cycle()
+                    .skip(pos)
+                    .zip(buf)
+                    .map(|(k, b)| k ^ b),
+            );
+            munged
+        };
 
         self.writer.write_all(&munged)?;
         Ok(buf.len())
@@ -109,5 +130,34 @@ mod tests {
 
         assert_ne!(intermediate, data.as_bytes());
         assert_eq!(xs2.munge(intermediate).collect::<Vec<_>>(), data.as_bytes());
+    }
+
+    #[test]
+    fn writer_roundtrip() {
+        let data =
+            "Spiderman! It's spiderman! Not a bird, or a plane, or a fireman! Just spiderman!";
+        let mut writer_dest = Vec::new();
+        let xs1 = Xorcism::new("Who knows what evil lurks in the hearts of men?".as_bytes());
+        let xs2 = xs1.clone();
+        {
+            let mut writer = xs1.writer(xs2.writer(&mut writer_dest));
+            assert!(writer.write_all(data.as_bytes()).is_ok());
+        }
+
+        assert_eq!(writer_dest, data.as_bytes());
+    }
+
+    #[test]
+    fn writer_munges() {
+        let data = "If wishes were horses, beggars would ride.";
+        let mut writer_dest = Vec::new();
+        {
+            let mut writer =
+                Xorcism::new("TRANSMUTATION_NOTES_1".as_bytes()).writer(&mut writer_dest);
+            assert!(writer.write_all(data.as_bytes()).is_ok());
+        }
+
+        assert_eq!(writer_dest.len(), data.len());
+        assert_ne!(writer_dest, data.as_bytes());
     }
 }
